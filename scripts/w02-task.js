678d94d31f0b4fe591c41454b71df48ee32e7611
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-01-25 18:44:09 -0500

    update

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -5,34 +5,37 @@
 /* Step 2 - Variables */
 let fullName = ("Kiara Mitchell");
 let currentYear = (2024);
-let profilePicture = ("images\profilepic.jpg");
+let profilePicture = ("images/profilepic.jpg");
 
 
 
 /* Step 3 - Element Variables */
 
 const nameElement = document.getElementById('name');
-var foodElement = document.getElementById('#food');
-var yearElement = document.querySelector('#year');
-var imageElement = document.querySelectorAll("img")
+let foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+let imageElement = document.querySelector("img");
 
 
 /* Step 4 - Adding Content */
 
+imageElement.setAttribute('src', `${profilePicture}`);
 nameElement.innerHTML = (`<strong>${fullName}</strong>`);
-yearElement.textContent = currentYear;
-imageElement.setAttribute('src', profilePicture);
-profilePicture.setAttribute("alt", `Profile image of ${fullName}`);
+yearElement.textContent = (`${currentYear}`);
+//profilePicture.setAttribute(`"alt", Profile image of ${fullName}`);
 
 /* Step 5 - Array */
 
 let foods = ["Panang Curry", "Tacos", "Salsa", "Anything Spicy"];
-foodElement.innerHTML = `<p>${foods.join(", ")}</p>`;
-let extraFood = 'Chocolate';
+
+foodElement.innerHTML += `<br>${foods}`;
+let extraFood = ('Chocolate');
 foods.push(extraFood);
-foodElement.innerHTML = `<p>${foods.join(", ")}</p>`;
-foods.pop()
-foodElement.innerHTML = `<p>${foods.join(", ")}</p>`;
+
+foods.shift();
+foodElement.innerHTML += (`<br>${foods}`);
+foods.pop();
+foodElement.innerHTML += (`<br>${foods}`);
 
 
 
