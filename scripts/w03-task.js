1f86810a4c2f9f369af214e58be848da0be3abf1
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-01-27 08:43:05 -0500

    week 3 ahhhh

---
+++ scripts/w03-task.js
@@ -0,0 +1,40 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum
+}
+function addNumbers() {
+    const num1 = document.getElementById("add1");
+    const num2 = document.querySelector("#add2");
+    return add(num1, num2);
+
+}
+document.getElementById("addNumbers").addEventListener('click', addNumbers);
+
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
