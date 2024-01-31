5f6721cd6e0f7edb4235831b19d0904b64e38219
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-01-31 15:53:47 -0500

    initial

---
+++ scripts/w04-task.js
@@ -0,0 +1,65 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+
+let profile = {
+    name: "Kiara Mitchell"
+    photo: {
+        src: "profilepic.jpg"
+        alt: "My Profile Picture"
+    },
+    favoriteFoods:{
+        'Salsa',
+        'Dragonfruit',
+        'Tacos',
+        'Chocolate'
+    },
+    hobbies: {
+        'crochet',
+        'writing',
+        'digital art',
+        'cooking'
+    },
+    placesLived: {
+        {
+            place: 'Las Vegas',
+            length: '19 years'
+        },
+        {
+            place: 'California',
+            length: '1 year'
+        },
+        {
+            place: 'Utah',
+            length: '6 years'
+        },
+        {
+            place: 'Florida',
+            length: '2 years'
+        }
+    }
+
+}
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
