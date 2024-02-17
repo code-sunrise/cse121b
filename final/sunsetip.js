679e0f611f2a99132b3846574ce3992b593449f6
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-02-16 22:04:34 -0500

    functions are working

--- final/sunsetip.js
+++ final/sunsetip.js
@@ -12,21 +12,29 @@ based on those coordinates and the time.*/
 
 /* global variables */
 var userInfo = [];
+var sunsetArray = [];
+var date = new Date();
+var tomorrow = date.setDate(date.getDate() +1)
+
+//use (lat, lng) => sunset array
+const solarEvents = async (lat, lon) => {
+    var solarAPI = "https://api.sunrise-sunset.org/json?";
+    var latitude = "lat=" + lat;
+    var longitude = "&lng=" + lon;
+    await fetch(solarAPI + latitude + longitude).then(response => response.json()).then(data => { sunsetArray.push(data) });
+};
 
 //fetch ip address
-const userData = () => {
-    fetch('https://ipapi.co/json').then(response => response.json()).then(data => {
+const userData = async () => {
+    await fetch('https://ipapi.co/json').then(response => response.json()).then(data => {
         userInfo.push(data);
     })
 }
+/*Make sure the UTC time works for the timezone the user is in */
 
-//use ip address to get lat and lng
 
-//turn (lat, lng) => utc offset
-
-//use (lat, lng) => sunset array
 
 /*event listener for the getElementById("button")
-
+//do i even need this anymore?
 take the input and run it through the functions*/
-userData()
+userData().then(() => solarEvents(userInfo[0].latitude, userInfo[0].longitude)), console.log(userInfo, sunsetArray);
