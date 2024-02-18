57292f639e789fc321f0338c3651e6f6ba3d21ab
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-02-17 20:03:45 -0500

    getting close

--- final/sunsetip.js
+++ final/sunsetip.js
@@ -13,15 +13,20 @@ based on those coordinates and the time.*/
 /* global variables */
 var userInfo = [];
 var sunsetArray = [];
-var date = new Date();
-var tomorrow = date.setDate(date.getDate() +1)
+var timesArray = [];
 
 //use (lat, lng) => sunset array
 const solarEvents = async (lat, lon) => {
     var solarAPI = "https://api.sunrise-sunset.org/json?";
     var latitude = "lat=" + lat;
     var longitude = "&lng=" + lon;
+    var date = new Date();
+    date.setDate(date.getDate() + 1);
+    var tomorrow = "&date=" + date.toISOString().split('T')[0];
+
     await fetch(solarAPI + latitude + longitude).then(response => response.json()).then(data => { sunsetArray.push(data) });
+
+    await fetch(solarAPI + latitude + longitude + tomorrow).then(response => response.json()).then(data => { sunsetArray.push(data) });
 };
 
 //fetch ip address
@@ -32,9 +37,103 @@ const userData = async () => {
 }
 /*Make sure the UTC time works for the timezone the user is in */
 
+const utcOffset = (utcTime, offset) => {
+    let [time, period] = utcTime.split(' ');
+    let [hours, minutes] = time.split(':').map(Number);
+    console.log(period, hours, minutes);
+    offsetSign = offset < 0 ? -1 : 1;
+    offsetHours = Number(offset.slice(0, -2));
+    offsetMinutes = Number(offset.slice(-2));
+    /* if the minute offset is 00, we can skip adding minutes*/
+    if (offsetMinutes > 0) {
+        offsetMinutes = offsetMinutes * offsetSign
+        minutes += offsetMinutes
+    }
+    if (minutes > 60) {
+        hours += 1
+        minutes -= 60
+    } else if (minutes < 0) {
+        hours -= 1
+        minutes += 60
+    }
+    hours = hours + offsetHours;
+    if (hours < 0) {
+        hours += 12;
+        if (period === 'AM') {
+            period = 'PM';
+        }
+        else if (period === 'PM') {
+            period = 'AM';
+        }
+    }
+    if (hours > 12) {
+        hours -= 12;
+        if (period === 'AM') {
+            period = 'PM';
+        }
+        else if (period === 'PM') {
+            period = 'AM';
+        }
+        timeObject = {
+            hours: hours,
+            minutes: minutes,
+            period: period
+        }
+        timesArray.push(timeObject)
+    }
+    /*if the hour offset is 00, we can skip adding minutes*/
+    /*if the offset is below 0, we can add 12 and switch the period*/
+    /*if the offset is above 12, we can subtract 12 and switch the period*/
+};
+const localTime = () => {
+    return new Promise((resolve, reject) => {
+        let now = new Date();
+        let currentHours = now.getUTCHours();
+        let currentMinutes = now.getUTCMinutes();
+        let currentPeriod = 'AM'
+        if (currentHours > 12) {
+            currentHours -= 12;
+            currentPeriod = 'PM'
+        }
+        let timeObject = {
+            hours: currentHours,
+            minutes: currentMinutes,
+            period: currentPeriod
+        }
+        timesArray.push(timeObject);
+       // console.log(timesArray)
 
-
-/*event listener for the getElementById("button")
-//do i even need this anymore?
-take the input and run it through the functions*/
-userData().then(() => solarEvents(userInfo[0].latitude, userInfo[0].longitude)), console.log(userInfo, sunsetArray);
+        //times
+        let sunrise = (sunsetArray[0].results.sunrise);
+        let sunset = (sunsetArray[0].results.sunset);
+        let sunriseTomorrow = (sunsetArray[1].results.sunrise);
+        console.log(sunrise, sunset, sunriseTomorrow)
+        utcOffset(sunrise, userInfo[0].utc_offset);
+        utcOffset(sunset, userInfo[0].utc_offset);
+        utcOffset(sunriseTomorrow, userInfo[0].utc_offset);
+        resolve();
+        console.log(timesArray)
+    });
+}
+const nearestTime = () => {
+    const eventElement = document.getElementById("event")
+    let heading = document.createElement("h2")
+    let condition = `Sunrise ${timesArray[3].hours}:${timesArray[3].minutes} ${timesArray[3].period}`
+    if (timesArray[0].period === timesArray[1].period) {
+        if (timesArray[0].hours < timesArray[1].hours) {
+            if (timesArray[0].minutes < timesArray[1].minutes) {
+                condition = `Sunrise ${timesArray[1].hours}:${timesArray[1].minutes} ${timesArray[1].period}`
+            }
+        }
+    }
+    else if (timesArray[0].hours < timesArray[2].hours) {
+        if (timesArray[0].minutes < timesArray[2].minutes) {
+            condition = `Sunrise ${timesArray[2].hours}:${timesArray[2].minutes} ${timesArray[2].period}`
+        }
+    }
+    heading.innerHTML = condition;
+    eventElement.appendChild(heading);
+}
+/*execute functions*/
+userData().then(() => solarEvents(userInfo[0].latitude, userInfo[0].longitude).then(() => localTime().then(() => nearestTime())));
+console.log(userInfo, sunsetArray, timesArray)
